96735d4355a67e6e01e28b7c793abfa354b49ba0
The Prattler <jmichaelsmith@juno.com>
2021-10-25 11:39:34 -0400

    captured form field values part 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,11 +14,12 @@ var createTaskHandler = function(event) {
 
     // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
+
     // give it a class name
     taskInfoEl.className = "task-info";
+    
     // add HTML content to div
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-
     listItemEl.appendChild(taskInfoEl);
 
     // add entire list item to list
