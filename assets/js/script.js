ce1a8faa26e00a054977d4d5210035a7cea259a0
The Prattler <jmichaelsmith@juno.com>
2021-10-26 11:31:21 -0400

    added ability to delete task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var pageContentEl = document.querySelector("#page-content");
 
 
 var taskFormHandler = function(event) {
@@ -101,4 +102,34 @@ var createTaskActions = function(taskId) {
     return actionContainerEl;
 };
 
-formEl.addEventListener("submit", taskFormHandler); 
+
+
+formEl.addEventListener("submit", taskFormHandler);
+
+var taskButtonHandler = function(event) {
+    console.log(event.target);
+
+    var deleteTask = function(taskId) {
+        var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+        taskSelected.remove();
+    };
+
+    if (event.target.matches(".delete-btn")) {
+        // get the element's task id
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+
+    
+
+    /*if (event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    };*/
+};
+
+
+
+
+
+pageContentEl.addEventListener("click", taskButtonHandler);
